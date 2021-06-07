ae8418ae52d5bdbe5b1ba3de74301cb1cab0a490
Eli Osherovich <eli.osherovich@gmail.com>
2021-06-07 17:27:59 +0300

    Typing&naming.

--- arxiv_2017_1711.09784/tree.py
+++ arxiv_2017_1711.09784/tree.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 import tensorflow as tf
 
 
@@ -17,25 +19,25 @@ class TreeModel(tf.keras.Model):
 class LeafNode(tf.keras.layers.Layer):
 
   def __init__(self, dim):
-    super().__init__()
+    super().__init__(name="leaf")
     self.logits = self.add_weight(name="logits", shape=(dim,), trainable=True)
 
-  def call(self, _inputs):
+  def call(self, _inputs: tf.Tensor) -> tf.Variable:
     return self.logits
 
 
 class InnerNode(tf.keras.layers.Layer):
 
   def __init__(self):
-    super().__init__()
+    super().__init__(name="inner_node")
     self.dense = tf.keras.layers.Dense(1, activation="sigmoid")
 
-  def call(self, inputs):
+  def call(self, inputs: tf.Tensor) -> tf.Tensor:
     pR = self.dense(inputs)
     return pR * self.right(inputs) + (1 - pR) * self.left(inputs)
 
 
-def build_tree(depth, n_classes):
+def build_tree(depth: int, n_classes: int) -> Union[LeafNode, InnerNode]:
   if depth == 1:
     return LeafNode(n_classes)
 
