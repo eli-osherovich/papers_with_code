15a16a9b1716f004ec85af3bb8193f66022b7bb7
Eli Osherovich <eli.osherovich@gmail.com>
2021-06-24 11:45:51 +0300

    Readability.

--- common/utils.py
+++ common/utils.py
@@ -3,22 +3,22 @@ import logging
 import numpy as np
 
 
-def roundrobin_generator(arr, batch=1, rng=np.random.default_rng()):
-  assert isinstance(
-    batch,
-    (int, np.integer)), f"Batch must be an integral type, got {type(batch)}"
-  assert batch > 0, f"Batch must be strictly positive, got {batch}"
+def roundrobin_generator(arr, batch_size=1, rng=np.random.default_rng()):
+  assert isinstance(batch_size, (int, np.integer)), \
+    f"Batch must be an integral type, got {type(batch_size)}"
+    
+  assert batch_size > 0, f"Batch must be strictly positive, got {batch_size}"
 
   arr_len = len(arr)
 
-  if batch > arr_len:
+  if batch_size > arr_len:
     logging.warning(
       "Batch size %d is larger than the number of elements in the sequence: %d",
-      batch,
+      batch_size,
       arr_len,
     )
 
-  multiplicity = np.ceil(batch / arr_len)
+  multiplicity = np.ceil(batch_size / arr_len)
   logging.info("Maximal element multiplicity in a batch: %d", multiplicity)
 
   all_indices = np.repeat(np.arange(arr_len), multiplicity)
@@ -27,12 +27,12 @@ def roundrobin_generator(arr, batch=1, rng=np.random.default_rng()):
 
   while True:
     start = 0
-    end = batch
+    end = batch_size
     used_idx_batches = []
     while end < all_indices_len:
       batch_idx = cur_perm[start:end]
       used_idx_batches.append(batch_idx)
       yield arr[cur_perm[start:end]]
-      start, end = end, end + batch
+      start, end = end, end + batch_size
     used_indices = np.concatenate(used_idx_batches)
     cur_perm = np.concatenate((cur_perm[start:], rng.permutation(used_indices)))
