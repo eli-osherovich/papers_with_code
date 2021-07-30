e04483b7e3079b24e89bd69688199f4ff81d0bb0
Eli Osherovich <eli.osherovich@gmail.com>
2021-07-30 21:00:10 +0300

    Moving from os.path to pathlib.

--- datasets/download.py
+++ datasets/download.py
@@ -1,23 +1,28 @@
-from functools import cache
-import os
 import pathlib
 import tempfile
+from typing import Optional, Union
 
 import tensorflow as tf
 
 _DATASETS_DIR = 'pwc-datasets'
 
 
-def download_dataset(ds_name: str, dl_config: dict, cache_dir=None):
+def download_dataset(
+  ds_name: str,
+  dl_config: dict[str, str],
+  cache_dir: Optional[Union[str, pathlib.Path]] = None,
+):
   if cache_dir is None:
-    cache_dir = os.path.join(tempfile.gettempdir(), _DATASETS_DIR)
+    cache_dir = pathlib.Path(tempfile.gettempdir()) / _DATASETS_DIR
+  else:
+    cache_dir = pathlib.Path(cache_dir)  # in case it is `str`.
 
   if ds_name not in dl_config:
     raise RuntimeError(
       f'Dataset name {ds_name} cannot be found in the download config {dl_config}'  # noqa E501
     )
 
-  os.makedirs(cache_dir, exist_ok=True)
+  cache_dir.mkdir(parents=True, exist_ok=True)
 
   url = dl_config[ds_name]['url']
   checksum = dl_config[ds_name].get('checksum')
