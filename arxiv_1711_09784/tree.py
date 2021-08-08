37b7f4d1a0ed784da9076e9441235da2328e8bec
Eli Osherovich <eli.osherovich@gmail.com>
2021-08-08 14:01:11 +0300

    Slight code refactoring.

--- arxiv_1711_09784/tree.py
+++ arxiv_1711_09784/tree.py
@@ -5,12 +5,12 @@ import tensorflow as tf
 
 class TreeModel(tf.keras.Model):
 
-  def __init__(self, depth, n_classes=10):
+  def __init__(self, n_classes: int, depth: int):
     super().__init__()
     self.model = tf.keras.Sequential(
       [
         tf.keras.layers.Flatten(),
-        build_tree(depth, n_classes),
+        build_tree(n_classes=n_classes, depth=depth),
       ]
     )
 
@@ -21,8 +21,8 @@ class TreeModel(tf.keras.Model):
 class LeafNode(tf.keras.layers.Layer):
 
   def __init__(self, dim):
-    super().__init__(name="leaf")
-    self.logits = self.add_weight(name="logits", shape=(dim,), trainable=True)
+    super().__init__(name='leaf')
+    self.logits = self.add_weight(name='logits', shape=(dim,), trainable=True)
 
   def call(self, _inputs: tf.Tensor) -> tf.Variable:
     return self.logits
@@ -31,19 +31,19 @@ class LeafNode(tf.keras.layers.Layer):
 class InnerNode(tf.keras.layers.Layer):
 
   def __init__(self):
-    super().__init__(name="inner_node")
-    self.dense = tf.keras.layers.Dense(1, activation="sigmoid")
+    super().__init__(name='inner_node')
+    self.dense = tf.keras.layers.Dense(1, activation='sigmoid')
 
   def call(self, inputs: tf.Tensor) -> tf.Tensor:
     pR = self.dense(inputs)
     return pR * self.right(inputs) + (1 - pR) * self.left(inputs)
 
 
-def build_tree(depth: int, n_classes: int) -> Union[LeafNode, InnerNode]:
+def build_tree(*, n_classes: int, depth: int) -> Union[LeafNode, InnerNode]:
   if depth == 1:
     return LeafNode(n_classes)
 
   root = InnerNode()
-  root.left = build_tree(depth - 1, n_classes)
-  root.right = build_tree(depth - 1, n_classes)
+  root.left = build_tree(n_classes=n_classes, depth=depth - 1)
+  root.right = build_tree(n_classes=n_classes, depth=depth - 1)
   return root
