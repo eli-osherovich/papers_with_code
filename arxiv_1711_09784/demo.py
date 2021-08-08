abe24adc1c58fc271b87d3459524dafd0b53c684
Eli Osherovich <eli.osherovich@gmail.com>
2021-08-08 14:00:02 +0300

    Moved to absl instead of argparse in arxiv_1711_09784.

--- arxiv_1711_09784/demo.py
+++ arxiv_1711_09784/demo.py
@@ -1,95 +1,80 @@
 #!/usr/bin/env python3
 
 # Python's relative import was invented by pure PERVERTS.
-if __name__ == "__main__" and __package__ is None:
-  import os
+if __name__ == '__main__' and __package__ is None:
   import sys
-  module_path = os.path.abspath(os.path.join(__file__, ".."))
-  module_name = os.path.basename(module_path)
-  pkg_path = os.path.abspath(os.path.join(__file__, "..", ".."))
-  pkg_name = os.path.basename(pkg_path)
-  sys.path.append(os.path.join(pkg_path, ".."))
-  __package__ = f"{pkg_name}.{module_name}"  # noqa A001
-
-import argparse
+  import pathlib
+  module_path = pathlib.Path(__file__).parent.resolve()
+  module_name = module_path.name
+  pkg_path = module_path.parent
+  pkg_name = pkg_path.name
+  sys.path.append(pkg_path.parent.as_posix())
+  __package__ = f'{pkg_name}.{module_name}'  # noqa: A001
 
 import tensorflow as tf
 import tensorflow_datasets as tfds
+from absl import app, flags
 
 from . import tree
 
+FLAGS = flags.FLAGS
 
-def parse_args():
-  parser = argparse.ArgumentParser()
-
-  parser.add_argument(
-    "--depth",
-    type=int,
-    default=9,
-    help="Tree depth (including root and leaves)"
-  )
-  parser.add_argument(
-    "--epochs", type=int, default=1000, help="Number of epochs"
-  )
-  parser.add_argument("--batch-size", type=int, default=1024, help="Batch size")
-  parser.add_argument(
-    "--shuffle-buffer", type=int, default=60000, help="Shuffle buffer size"
-  )
-  parser.add_argument(
-    "--patience", type=int, default=10, help="Early stopping patience"
-  )
-  parser.add_argument(
-    "--keep-last",
-    default=False,
-    dest="keep_last",
-    action="store_true",
-    help="Keep last model (by default) the best model is kept"
-  )
-
-  args = parser.parse_args()
-  return args
+flags.DEFINE_string('dataset', 'mnist', 'Dataset to load')
+flags.DEFINE_integer(
+  'depth', 9, 'Tree depth (including root and leaves)', lower_bound=2
+)
+flags.DEFINE_integer('epochs', 1000, 'Number of epochs', lower_bound=0)
+flags.DEFINE_integer('batch_size', 1024, 'Batch size', lower_bound=1)
+flags.DEFINE_integer('shuffle_buffer', 60000, 'Shuffle buffer size')
+flags.DEFINE_integer('patience', 10, 'Early stopping patience')
+flags.DEFINE_bool(
+  'keep_last', False, 'Keep last model (default is to keep the best model)'
+)
 
 
-def main():
-  args = parse_args()
-
-  train_ds, val_ds = tfds.load(
-    "mnist", split=["train", "test"], shuffle_files=False, as_supervised=True
+def main(argv):
+  (train_ds, val_ds), info = tfds.load(
+    FLAGS.dataset,
+    split=['train', 'test'],
+    shuffle_files=False,
+    as_supervised=True,
+    with_info=True,
   )
 
   train_ds = train_ds\
     .map(lambda x, y: (x / 255, y), num_parallel_calls=tf.data.AUTOTUNE)\
     .cache()\
-    .shuffle(args.shuffle_buffer)\
-    .batch(args.batch_size)\
+    .shuffle(FLAGS.shuffle_buffer)\
+    .batch(FLAGS.batch_size)\
     .prefetch(tf.data.AUTOTUNE)
 
   val_ds = val_ds\
     .map(lambda x, y: (x / 255, y), num_parallel_calls=tf.data.AUTOTUNE)\
-    .batch(args.batch_size)\
+    .batch(FLAGS.batch_size)\
     .cache()\
     .prefetch(tf.data.AUTOTUNE)
 
-  model = tree.TreeModel(args.depth)
+  n_classes = info.features[info.supervised_keys[-1]].num_classes
+  model = tree.TreeModel(n_classes=n_classes, depth=FLAGS.depth)
 
   early_stop = tf.keras.callbacks.EarlyStopping(
-    monitor="val_acc",
-    patience=args.patience,
-    restore_best_weights=not args.keep_last
+    monitor='val_acc',
+    patience=FLAGS.patience,
+    restore_best_weights=not FLAGS.keep_last
   )
 
   model.compile(
     loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    metrics=["acc"]
+    metrics=['acc']
   )
   model.fit(
     train_ds,
-    epochs=args.epochs,
+    epochs=FLAGS.epochs,
     validation_data=val_ds,
     callbacks=[early_stop]
   )
-  model.save("model")
+  model.save('model')
 
 
-if __name__ == "__main__":
-  main()
+if __name__ == '__main__':
+  app.run(main)
