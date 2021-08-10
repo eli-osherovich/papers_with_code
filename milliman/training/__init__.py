489a9c2c3e38a101d5e7650e0def1561034fd953
Eli Osherovich <eli.osherovich@gmail.com>
2021-08-10 21:48:26 +0300

    Added action flag ('train' or 'train_cv') to training.

--- milliman/training/__init__.py
+++ milliman/training/__init__.py
@@ -1,4 +1,2 @@
 # flake8: noqa F401
 from .training import train
-from .tree_training import tree_train
-from .xgb_training import xgb_train
--- milliman/training/training.py
+++ milliman/training/training.py
@@ -9,10 +9,31 @@ flags.DEFINE_enum_class(
   'model', model.MODEL.TREE, model.MODEL, 'Model to train'
 )
 
+flags.DEFINE_enum('action', 'train', ['train', 'train_cv'], 'Action to perform')
+
+
+def _get_action():
+  if FLAGS.action == 'train':
+    return 'train'
+  elif FLAGS.action == 'train_cv':
+    return 'train_cv'
+  else:
+    raise RuntimeError('Unknown action: %s', FLAGS.action)
+
+
+def _get_model_module():
+  if FLAGS.model == model.MODEL.TREE:
+    return tree_training
+  elif FLAGS.model == model.MODEL.XGB:
+    return xgb_training
+  else:
+    raise RuntimeError('Unknown model: %s', FLAGS.model)
+
 
 def train():
   X, y = data.get_numpy()
-  if FLAGS.model == model.MODEL.TREE:
-    return tree_training.tree_train(X, y)
-  elif FLAGS.model == model.MODEL.XGB:
-    return xgb_training.xgb_train(X, y)
+
+  model_module = _get_model_module()
+  action = _get_action()
+  train_fn = getattr(model_module, action)
+  return train_fn(X, y)
