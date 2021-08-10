8c77499174297ea555d3018ddff0ad850fb852df
Eli Osherovich <eli.osherovich@gmail.com>
2021-08-10 21:47:19 +0300

    Added 'n_jobs' parameter to XGB model

--- milliman/model/xgb_model.py
+++ milliman/model/xgb_model.py
@@ -8,6 +8,7 @@ GIN_CONFIG_FILE = 'xgb_model_config.gin'
 
 @gin.configurable
 def get_xgb_model(
+  *,
   n_estimators,
   objective,
   colsample_bytree,
@@ -17,6 +18,7 @@ def get_xgb_model(
   scale_pos_weight,
   subsample,
   random_state,
+  n_jobs,
 ):
   return xgb.XGBClassifier(
     n_estimators=n_estimators,
@@ -28,6 +30,8 @@ def get_xgb_model(
     scale_pos_weight=scale_pos_weight,
     subsample=subsample,
     random_state=random_state,
+    n_jobs=n_jobs,
+    use_label_encoder=False,
   )
 
 
