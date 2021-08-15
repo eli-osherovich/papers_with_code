6b5bd59f8acb6e6d99d31822043f4b019215584b
Eli Osherovich <eli.osherovich@gmail.com>
2021-08-15 16:00:10 +0300

    Added tree leaf initializer option.

--- arxiv_1711_09784/tree.py
+++ arxiv_1711_09784/tree.py
@@ -1,16 +1,20 @@
-from typing import Union
+from typing import Union, Optional
 
 import tensorflow as tf
 
 
 class TreeModel(tf.keras.Model):
 
-  def __init__(self, n_classes: int, depth: int):
+  def __init__(
+    self, n_classes: int, depth: int, leaf_initializer: Optional[float] = None
+  ):
     super().__init__()
     self.model = tf.keras.Sequential(
       [
         tf.keras.layers.Flatten(),
-        build_tree(n_classes=n_classes, depth=depth),
+        build_tree(
+          n_classes=n_classes, depth=depth, leaf_initializer=leaf_initializer
+        ),
       ]
     )
 
@@ -20,9 +24,14 @@ class TreeModel(tf.keras.Model):
 
 class LeafNode(tf.keras.layers.Layer):
 
-  def __init__(self, dim):
+  def __init__(self, *, dim, init_value=None):
     super().__init__(name='leaf')
-    self.logits = self.add_weight(name='logits', shape=(dim,), trainable=True)
+    if init_value is not None:
+      init_value = tf.keras.initializers.Constant(init_value)
+
+    self.logits = self.add_weight(
+      name='logits', shape=(dim,), trainable=True, initializer=init_value
+    )
 
   def call(self, _inputs: tf.Tensor) -> tf.Variable:
     return self.logits
@@ -39,9 +48,14 @@ class InnerNode(tf.keras.layers.Layer):
     return pR * self.right(inputs) + (1 - pR) * self.left(inputs)
 
 
-def build_tree(*, n_classes: int, depth: int) -> Union[LeafNode, InnerNode]:
+def build_tree(
+  *,
+  n_classes: int,
+  depth: int,
+  leaf_initializer: Optional[float] = None
+) -> Union[LeafNode, InnerNode]:
   if depth == 1:
-    return LeafNode(n_classes)
+    return LeafNode(dim=n_classes, init_value=leaf_initializer)
 
   root = InnerNode()
   root.left = build_tree(n_classes=n_classes, depth=depth - 1)
