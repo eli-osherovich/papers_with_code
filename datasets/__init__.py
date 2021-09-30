d7d20c36014dfda124a5ad5a3044497b36d4c4d2
Eli Osherovich <eli.osherovich@gmail.com>
2021-09-30 10:01:52 +0300

    Export movielens datasets.

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,2 +1,4 @@
-from .kddcup99 import KddCup99  # noqa F401
-from .milliman import MillimanDataset, MillimanDataset2  # noqa F401
+# pylint: disable=F401
+from .kddcup99 import KddCup99
+from .milliman import MillimanDataset, MillimanDataset2
+from .movielens import Movielens100K, Movielens1M, Movielens10M, Movielens20M, Movielens25M, MovielensLatest, MovielensLatestSmall
