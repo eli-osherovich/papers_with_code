209ac0bf2db0a9fab7a1be81d9c50492a6412812
Eli Osherovich <eli.osherovich@gmail.com>
2021-11-07 15:04:01 +0200

    Manual corrections to the automatically generated diabetes dataset.

--- datasets/diabetes/dl_config.py
+++ datasets/diabetes/dl_config.py
@@ -4,9 +4,9 @@ from .. import utils
 
 DATASETS = {
   "train": {
-    "uri": utils.make_uri("diabetes.csv"),
-    "checksum": "FIXME: add real checksum",
+    "uri":
+      utils.make_uri("diabetes.csv"),
+    "checksum":
+      "9af41baf65fe75e3e76283978180c9399dcc167656377219b64b793503ce7c76",
   },
-  
-  
 }
--- datasets/diabetes/ds_config.py
+++ datasets/diabetes/ds_config.py
@@ -1,6 +1,5 @@
 """Diabetes dataset schema.
 """
-from absl import logging
 import numpy as np
 import pandas as pd
 
@@ -9,7 +8,6 @@ _FLOAT_TYPE = np.float32
 _BOOL_TYPE = np.int32
 _CATEGORICAL_TYPE = pd.CategoricalDtype
 
-logging.error("Please review the schema below and remove this exception")
 feature_dict = {
   # Example:
   #
@@ -20,16 +18,15 @@ feature_dict = {
   #   "human",
   #   "unknown",
   # ])
-  
-  "feature0": _FLOAT_TYPE,
-  "feature1": _FLOAT_TYPE,
-  "feature2": _FLOAT_TYPE,
-  "feature3": _FLOAT_TYPE,
-  "feature4": _FLOAT_TYPE,
-  "feature5": _FLOAT_TYPE,
-  "feature6": _FLOAT_TYPE,
-  "feature7": _FLOAT_TYPE,
-  "feature8": _FLOAT_TYPE,
-  "feature9": _FLOAT_TYPE,
+  "age": _FLOAT_TYPE,
+  "sex": _FLOAT_TYPE,
+  "bmi": _FLOAT_TYPE,
+  "bp": _FLOAT_TYPE,
+  "s1 tc": _FLOAT_TYPE,
+  "s2 ldl": _FLOAT_TYPE,
+  "s3 hdl": _FLOAT_TYPE,
+  "s4 tch": _FLOAT_TYPE,
+  "s5 ltg": _FLOAT_TYPE,
+  "s6 glu": _FLOAT_TYPE,
   "target": _FLOAT_TYPE,
 }
