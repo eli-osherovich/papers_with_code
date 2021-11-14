7b99747eec94b780ac58847581b3da670b126d0e
Eli Osherovich <eli.osherovich@gmail.com>
2021-11-14 12:09:50 +0200

    Updated cookiecutter with the new dataset type.

--- cookiecutter/dataset/{{cookiecutter.dataset_module}}/{{cookiecutter.dataset_module}}.py
+++ cookiecutter/dataset/{{cookiecutter.dataset_module}}/{{cookiecutter.dataset_module}}.py
@@ -5,10 +5,11 @@
 """
 
 from .. import dataset
+from ..typing import DatasetType
 
 
 class {{cookiecutter.dataset_class}}(dataset.Dataset):
   """{{cookiecutter.dataset}} dataset."""
 
   def __init__(self) -> None:
-    super().__init__(target_columns=[{{string_to_quoted_list(cookiecutter.target_columns)}}])
+    super().__init__(type=DatasetType.{{cookiecutter.dataset_type|upper}}, target_columns=[{{string_to_quoted_list(cookiecutter.target_columns)}}])
