7b039a30ce625fdd412fe2be0e95b6c7f1684abd
Eli Osherovich <eli.osherovich@gmail.com>
2021-11-15 14:08:34 +0200

    TabNet training match the new data loading.

--- arxiv_1908_07442/training/tabnet_training.py
+++ arxiv_1908_07442/training/tabnet_training.py
@@ -3,7 +3,6 @@
 """
 import gin
 from sklearn import model_selection
-from sklearn import preprocessing
 import tensorflow as tf
 
 from .. import models
@@ -20,28 +19,9 @@ def _train_model(
 
 
 @gin.configurable
-def train(
-  X,
-  y,
-  *,
-  shuffle_buffer=163840,
-  batch_size: int,
-  test_size: float,
-):
-  le = preprocessing.LabelEncoder()
-  y = le.fit_transform(y)
-  x_train, x_val, y_train, y_val = model_selection.train_test_split(
-    X, y, test_size=test_size, stratify=y
-  )
-  train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train)
-                                               ).shuffle(shuffle_buffer).batch(
-                                                 batch_size,
-                                                 drop_remainder=True
-                                               )
-  val_ds = tf.data.Dataset.from_tensor_slices((x_val, y_val)).batch(
-    batch_size, drop_remainder=True
-  )
-  fit_params = {"epochs": 500}
+def train(data):
+  train_ds, val_ds = get_ds(data)
+  fit_params = {"epochs": 100000, "validation_freq": 1}
 
   model_ = models.get_model()
   model_ = _train_model(model_, train_ds, val_ds=val_ds, fit_params=fit_params)
@@ -56,3 +36,24 @@ def train_cv(X, y):
 @gin.configurable
 def tune(X, y):
   raise NotImplementedError()
+
+
+@gin.configurable
+def get_ds(data, test_size=0.25, shuffle_buffer=16384, batch_size=4096):
+  train_data = data["train"]
+  if "val" in data:
+    val_data = data["val"]
+  else:
+    x_train, x_val, y_train, y_val = model_selection.train_test_split(
+      *train_data, test_size=test_size, stratify=train_data[1]
+    )
+    train_data = (x_train, y_train)
+    val_data = (x_val, y_val)
+
+  train_ds = tf.data.Dataset.from_tensor_slices(train_data)\
+      .shuffle(shuffle_buffer)\
+      .batch(batch_size, drop_remainder=True)
+  val_ds = tf.data.Dataset.from_tensor_slices(val_data)\
+    .shuffle(shuffle_buffer)\
+    .batch(batch_size, drop_remainder=True)
+  return train_ds, val_ds
