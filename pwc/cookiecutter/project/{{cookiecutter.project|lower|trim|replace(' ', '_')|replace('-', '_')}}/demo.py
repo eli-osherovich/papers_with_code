b43a5e17110943e8dcdd4b1ba92c359304be4210
Eli Osherovich <eli.osherovich@gmail.com>
2021-11-21 19:05:59 +0200

    Cookicutter

--- pwc/cookiecutter/project/{{cookiecutter.project|lower|trim|replace(' ', '_')|replace('-', '_')}}/demo.py
+++ pwc/cookiecutter/project/{{cookiecutter.project|lower|trim|replace(' ', '_')|replace('-', '_')}}/demo.py
@@ -1,26 +1,12 @@
 #!/usr/bin/env python3
 
-# Python's relative import was invented by pure PERVERTS.
-if __name__ == "__main__" and __package__ is None:
-  import os
-  import pathlib
-  import sys
-
-  module_path = pathlib.Path(__file__).parent.resolve()
-  module_name = module_path.name
-  pkg_path = module_path.parent
-  pkg_name = pkg_path.name
-  sys.path.append(pkg_path.parent.as_posix())
-  os.environ["PYTHONPATH"] = pkg_path.parent.as_posix()
-  __package__ = f"{pkg_name}.{module_name}"  # noqa: A001
-
 from absl import app
 from absl import flags
 import numpy as np
 import tensorflow as tf
 
-from ..common import utils
-from . import training
+from pwc.common import utils
+from pwc.{{cookiecutter.project|lower|trim|replace(" ", "_")|replace("-", "_")}} import training
 
 FLAGS = flags.FLAGS
 
