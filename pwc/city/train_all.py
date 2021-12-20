805ef09617c11135baa0a391139a135d1086f1e7
Eli Osherovich <eli.osherovich@gmail.com>
2021-12-20 09:20:40 +0200

    Added a new full flow for City's defaut
    The results are as follows:
    [04:28:31] Blending: optimization starts with equal weights and score 0.794047905984016
    [04:28:34] Blending: iteration 0: score = 0.7968499771687324, weights = [0.28986135 0.10888586 0.15513167 0.32493776 0. 0.05658842 0.06459495]
    [04:28:38] Blending: iteration 1: score = 0.7968642468099838, weights = [0.28139243 0.07923866 0.15863876 0.36317480 0. 0.05768685 0.05986851]
    [04:28:41] Blending: iteration 2: score = 0.7968649923991791, weights = [0.28562352 0.07582421 0.16316104 0.35883950 0. 0.05926304 0.05728873]
    [04:28:44] Blending: iteration 3: score = 0.7968654031065826, weights = [0.28784096 0.07806534 0.16285920 0.35570180 0. 0.05874484 0.05678779]
    [04:28:47] Blending: iteration 4: score = 0.7968654046019833, weights = [0.28784450 0.07806630 0.16284896 0.35570620 0. 0.05874557 0.05678849]
    [2021-12-20 04:29:55+0200] INFO - prefect | OOF score: 0.7968668615620872
    [2021-12-20 04:29:55+0200] INFO - prefect | TEST score: 0.7987260377785583

--- pwc/city/train_all.py
+++ pwc/city/train_all.py
@@ -1,18 +1,33 @@
 #!/usr/bin/env python
 
+from datetime import datetime
+import pathlib
+import random
+from typing import Any
+
 from absl import app
 from absl import flags
-import lightautoml.tasks
+import joblib
 from lightautoml.automl.base import AutoML
+from lightautoml.automl.blend import WeightedBlender
+from lightautoml.ml_algo.boost_cb import BoostCB
 from lightautoml.ml_algo.boost_lgbm import BoostLGBM
-from lightautoml.pipelines.features.lgb_pipeline import LGBSimpleFeatures
+from lightautoml.ml_algo.linear_sklearn import LinearLBFGS
+from lightautoml.ml_algo.tuning.optuna import OptunaTuner
+from lightautoml.pipelines.features.lgb_pipeline import LGBAdvancedPipeline, LGBSimpleFeatures
+from lightautoml.pipelines.features.linear_pipeline import LinearFeatures
 from lightautoml.pipelines.ml.base import MLPipeline
 from lightautoml.reader.base import PandasToPandasReader
+import lightautoml.tasks
+import numpy as np
 import pandas as pd
 from prefect import Flow
 from prefect import task
 from prefect.utilities import logging
+import psutil
 from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import train_test_split
+import torch
 
 from pwc.city import task_lib
 from pwc.datasets import HomeCreditDefaultRisk as Dataset
@@ -25,8 +40,25 @@ flags.DEFINE_bool(
 flags.DEFINE_bool("save", True, "Save results")
 
 flags.DEFINE_float("corr_thresh", 0.98, "Correlation threshold", lower_bound=0)
-flags.DEFINE_float("imp_thresh", 0.02, "Importance threshold", lower_bound=0)
+flags.DEFINE_float("imp_thresh", 0.005, "Importance threshold", lower_bound=0)
+flags.DEFINE_float(
+  "imp_thresh_scale", 10, "Importance threshold", lower_bound=1
+)
 flags.DEFINE_float("null_thresh", 0.6, "NULLity threshold", lower_bound=0)
+flags.DEFINE_float("lr", 0.025, "Learning rate", lower_bound=1e-10)
+flags.DEFINE_integer("cv_folds", 5, "Number of tuning trials", lower_bound=2)
+flags.DEFINE_integer(
+  "max_trees", 5000, "Maximal number of trees for boosters", lower_bound=1
+)
+flags.DEFINE_integer(
+  "num_leaves", 128, "Number of tuning trials", lower_bound=2
+)
+
+flags.DEFINE_integer("patience", 50, "Patience during early stopping")
+flags.DEFINE_integer(
+  "timeout", 3600 * 12, "Trials timeout (seconds)", lower_bound=1
+)
+flags.DEFINE_integer("trials", 150, "Number of tuning trials", lower_bound=1)
 
 _logger = logging.get_logger()
 
@@ -35,6 +67,7 @@ remove_high_corr = task(task_lib.remove_high_corr)
 load_features_data = task(task_lib.load_features_data)
 save_model = task(task_lib.save_model)
 get_important_features = task(task_lib.get_important_features)
+set_nans = task(task_lib.set_nans)
 
 
 @task
@@ -60,49 +93,216 @@ def merge_features(
 
 
 @task
-def train(train_df: pd.DataFrame, *, logger=None):
-  task = lightautoml.tasks.Task("binary")
-  reader = PandasToPandasReader(task, cv=5, random_state=13)
-  roles = {"target": "TARGET", "drop": "SK_ID_CURR"}
-
-  model = BoostLGBM(
-    default_params={
-      "boosting": "gbdt",
-      "early_stopping_rounds": 50,
-      "num_threads": 8,
-      "random_state": 0,
-    }
-  )
-  pipeline = MLPipeline([model], features_pipeline=LGBSimpleFeatures())
-  automl = AutoML(reader, [[pipeline]])
-  oof_pred = automl.fit_predict(train_df, roles=roles, verbose=2)
+def train(model, train_df, test_df, config, *, logger=None):
+  roles = {"target": "TARGET"}
+  oof_pred = model.fit_predict(train_df, roles=roles, verbose=2)
+  test_pred = model.predict(test_df)
 
   if logger:
     logger.info(
-      f"OOF score: {roc_auc_score(train_df['TARGET'].to_numpy(), oof_pred.data[:, 0])}"
+      f"OOF score: "
+      f"{roc_auc_score(train_df[config['target']].values, oof_pred.data[:, 0])}"
     )
 
-  return automl
+    logger.info(
+      f"TEST score: "
+      f"{roc_auc_score(test_df[config['target']].values, test_pred.data[:, 0])}"
+    )
+  timestamp = datetime.now().isoformat(timespec='minutes')
+  model_dir = pathlib.Path(__file__).parent / "models"
+  joblib.dump(model, model_dir / f"{timestamp}.pkl")
+
+
+@task
+def init(config: dict[str, Any]) -> None:
+  np.random.seed(config["random_state"])
+  torch.manual_seed(config["random_state"])
+  random.seed(config["random_state"])
+  torch.set_num_threads(config["n_threads"])
+
+
+@task
+def load_config(*, logger=None) -> dict[str, Any]:
+  config = {
+    "target": "TARGET",
+    "n_threads": psutil.cpu_count(logical=True),
+    "test_size": 0.1,
+    "random_state": 42,
+    "cv_folds": FLAGS.cv_folds,
+    "timeout": FLAGS.timeout,
+  }
+  if logger:
+    logger.info(f"Setting target column to {config['target']}")
+    logger.info(f"Setting number of threads to {config['n_threads']}")
+    logger.info(f"Setting number of CV folds to {config['cv_folds']}")
+  return config
+
+
+@task
+def first_level_pipeline(config) -> list[MLPipeline]:
+  kaggle_model = BoostLGBM(
+    name="KModel",
+    default_params={
+      "boosting": "gbdt",
+      "early_stopping_rounds": 200,
+      "num_threads": config["n_threads"],
+      "random_state": 0,
+      "learning_rate": 0.02,  # 02,
+      "num_leaves": 20,
+      "colsample_bytree": 0.9497036,
+      "subsample": 0.8715623,
+      "subsample_freq": 1,
+      "max_depth": 8,
+      "reg_alpha": 0.041545473,
+      "reg_lambda": 0.0735294,
+      "min_split_gain": 0.0222415,
+      "min_child_weight": 60,  # 39.3259775,
+    }
+  )
+  gbt_model = BoostLGBM(
+    name="LGBM",
+    default_params={
+      "boosting": "gbdt",
+      "early_stopping_rounds": FLAGS.patience,
+      "learning_rate": FLAGS.lr,
+      "num_leaves": FLAGS.num_leaves,
+      "num_threads": config["n_threads"],
+      "num_trees": FLAGS.max_trees,
+      "random_state": 0,
+    }
+  )
+
+  dart_model = BoostLGBM(
+    name="DART",
+    default_params={
+      "boosting": "dart",
+      # "early_stopping_rounds": FLAGS.patience, Not supported in DART
+      "learning_rate": FLAGS.lr,
+      "num_leaves": FLAGS.num_leaves,
+      "num_threads": config["n_threads"],
+      "num_trees": FLAGS.max_trees,
+      "random_state": 1,
+    }
+  )
+
+  rf_model = BoostLGBM(
+    name="RF",
+    default_params={
+      "boosting": "rf",
+      "early_stopping_rounds": FLAGS.patience,
+      "learning_rate": FLAGS.lr,
+      "num_leaves": FLAGS.num_leaves,
+      "num_threads": config["n_threads"],
+      "num_trees": FLAGS.max_trees,
+      "random_state": 2,
+    }
+  )
+
+  goss_model = BoostLGBM(
+    name="GOSS",
+    default_params={
+      "boosting": "goss",
+      "bagging_freq": 0,  # GOSS does not support bagging.
+      "early_stopping_rounds": FLAGS.patience,
+      "learning_rate": FLAGS.lr,
+      "num_leaves": FLAGS.num_leaves,
+      "num_threads": config["n_threads"],
+      "num_trees": FLAGS.max_trees,
+      "random_state": 3,
+    }
+  )
+
+  cb_model = BoostCB(
+    default_params={
+      "learning_rate": FLAGS.lr,
+      "num_trees": FLAGS.max_trees,
+      "random_state": 4,
+      "thread_count": config["n_threads"],
+    }
+  )
+
+  gbt_pipeline = MLPipeline(
+    [
+      kaggle_model,
+      gbt_model,
+      # (
+      #   gbt_model,
+      #   OptunaTuner(
+      #     n_trials=FLAGS.trials,
+      #     timeout=config["timeout"],
+      #     fit_on_holdout=False
+      #   )
+      # ),
+      cb_model,
+      dart_model,
+      rf_model,
+      goss_model,
+    ],
+    features_pipeline=LGBAdvancedPipeline(),
+  )
+
+  linear_pipeline = MLPipeline(
+    [LinearLBFGS()],
+    features_pipeline=LinearFeatures(),
+  )
+  return [
+    gbt_pipeline,
+    linear_pipeline,
+  ]
+
+
+@task
+def get_model(pipelines, config):
+  task = lightautoml.tasks.Task("binary")
+  reader = PandasToPandasReader(
+    task, cv=config["cv_folds"], random_state=config["random_state"]
+  )
+
+  return AutoML(reader, pipelines, blender=WeightedBlender())
+
+
+@task
+def split_data(df: pd.DataFrame,
+               config: dict[str, Any],
+               *,
+               logger=None) -> tuple[pd.DataFrame, pd.DataFrame]:
+  df.reset_index().to_feather("/tmp/full_df.feather")
+  train_df, test_df = train_test_split(
+    df,
+    test_size=config["test_size"],
+    random_state=config["random_state"],
+    stratify=df[config["target"]]
+  )
+  if logger:
+    logger.info(
+      f"Splitted data train/test: {1-config['test_size']}/{config['test_size']}"
+    )
+  return train_df, test_df
 
 
 def main(argv):
   del argv  # unused
 
   with Flow("main") as flow:
+    config = load_config()
+    init(config)
     all_feats = zip(
       [
-        "train", "train", "train", "credit_card_balance",
+        "train", "manual", "train", "train", "credit_card_balance",
         "installments_payments", "pos_cache_balance"
       ],
       [
-        None, "autofeat_num", "autofeat_bool", "autofeat_ts", "autofeat_ts",
-        "autofeat_ts"
+        None, None, "autofeat_num", "autofeat_bool", "autofeat_ts",
+        "autofeat_ts", "autofeat_ts"
       ],
     )
     train_df = load_targets()
     for data_type, features_type in all_feats:
+      imp_thresh = FLAGS.imp_thresh
+      if features_type == "autofeat_ts":
+        imp_thresh *= FLAGS.imp_thresh_scale
       imp_features = get_important_features(
-        data_type, features_type, FLAGS.imp_thresh, logger=_logger
+        data_type, features_type, imp_thresh, logger=_logger
       )
       features_df = load_features_data(
         data_type, features_type, cols=imp_features, logger=_logger
@@ -114,9 +314,14 @@ def main(argv):
         features_df, FLAGS.corr_thresh, logger=_logger
       )
       train_df = merge_features(train_df, features_df, logger=_logger)
-    model = train(train_df, logger=_logger)
-    # if FLAGS.save:
-    #   save_model(model, FLAGS.data_type, FLAGS.features_type)
+    train_df = set_nans(train_df)
+    train_df = remove_high_corr(train_df, FLAGS.corr_thresh, logger=_logger)
+    train_df, test_df = split_data(train_df, config)
+    level1 = first_level_pipeline(config)
+    # level2 = second_level_pipeline(config)
+    model = get_model([level1], config)
+    train(model, train_df, test_df, config, logger=_logger)
+
   flow.run()
 
 
