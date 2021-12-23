626dac195f91ce5cd8257e49d5ffc6e5775f647b
Eli Osherovich <eli.osherovich@gmail.com>
2021-12-23 21:56:54 +0200

    Updated City's fraud flow. The results are as follows:
    [2021-12-23 21:25:25+0200] INFO - prefect | OOF score: 0.9997482135410403
    [2021-12-23 21:25:25+0200] INFO - prefect | TEST score: 0.999195043385533

--- pwc/city/fraud_flow.py
+++ pwc/city/fraud_flow.py
@@ -1,167 +1,181 @@
+#!/usr/bin/env python
+
 import random
 from typing import Any
 
+from absl import app
 from absl import flags
-import lightautoml
-from lightautoml.automl.base import AutoML
 from lightautoml.dataset.roles import CategoryRole
-from lightautoml.dataset.roles import DatetimeRole
 from lightautoml.dataset.roles import TargetRole
 from lightautoml.ml_algo.boost_cb import BoostCB
 from lightautoml.ml_algo.boost_lgbm import BoostLGBM
 from lightautoml.ml_algo.linear_sklearn import LinearLBFGS
 from lightautoml.ml_algo.tuning.optuna import OptunaTuner
 from lightautoml.pipelines.features.lgb_pipeline import LGBAdvancedPipeline
-from lightautoml.pipelines.features.lgb_pipeline import LGBSimpleFeatures
 from lightautoml.pipelines.features.linear_pipeline import LinearFeatures
 from lightautoml.pipelines.ml.base import MLPipeline
-from lightautoml.reader.base import PandasToPandasReader
 import numpy as np
 import pandas as pd
 import prefect
 from prefect import Flow
 from prefect import task
-from prefect.engine.results import LocalResult
+from prefect.utilities import logging
 import psutil
-from sklearn.metrics import roc_auc_score
-from sklearn.model_selection import train_test_split
-import tensorflow as tf
 import torch
 
+from pwc.city import task_lib
 from pwc.datasets import SyntheticFraudDetection
 
 FLAGS = flags.FLAGS
 
+flags.DEFINE_bool("save", True, "Save results")
+
+flags.DEFINE_float("lr", 0.025, "Learning rate", lower_bound=1e-10)
+flags.DEFINE_integer("cv_folds", 5, "Number of tuning trials", lower_bound=2)
+flags.DEFINE_integer(
+  "max_trees", 5000, "Maximal number of trees for boosters", lower_bound=1
+)
+flags.DEFINE_integer(
+  "num_leaves", 128, "Number of tuning trials", lower_bound=2
+)
+
+flags.DEFINE_integer("patience", 50, "Patience during early stopping")
+flags.DEFINE_integer(
+  "timeout", 3600 * 12, "Trials timeout (seconds)", lower_bound=1
+)
+flags.DEFINE_integer("trials", 150, "Number of tuning trials", lower_bound=1)
+flags.DEFINE_float("test_size", 0.1, "Test size")
+
+_logger = logging.get_logger()
+_target = "isFraud"
+
+
+def task(func):
+  return func
+
+
+split_data = task(task_lib.split_data)
+train = task(task_lib.train)
+get_automl = task(task_lib.get_automl)
+
 
 @task
-def load_data() -> tuple[pd.DataFrame, dict]:
+def load_data() -> pd.DataFrame:
   ds = SyntheticFraudDetection()
-  df = ds.as_dataframe("train")
-  roles = {"target": "isFraud"}
-  return df, roles
+  return ds.as_dataframe("train")
 
 
 @task
 def feature_engineering(df: pd.DataFrame) -> tuple[pd.DataFrame, dict]:
-  df["MaxOut"] = df.amount == df.oldbalanceOrg
+  df["WithdrawAll"] = df.amount == df.oldbalanceOrg
+  df["OneMillion"] = df.amount == 1_000_000
   df["TransactionType"] = (
     df.nameOrig.apply(lambda x: x[0]) + "->" +
     df.nameDest.apply(lambda x: x[0])
   )
-  df["Timestamp"] = df.step.apply(
-    lambda s: pd.Timestamp("2021") + pd.Timedelta(f"{s}h")
-  )
+  # df["Timestamp"] = df.step.apply(
+  #   lambda s: pd.Timestamp("2021") + pd.Timedelta(f"{s}h")
+  # )
   df = df.drop(["nameOrig", "nameDest", "type", "step"], axis=1)
   roles = {
-    TargetRole(): "isFraud",
-    DatetimeRole(seasonality=["wd", "hour"], country="Nigeria"): "Timestamp",
-    CategoryRole(str): "TransactionType",
-    CategoryRole(np.int32, label_encoded=True): ["MaxOut", "MillionDollar"]
+    TargetRole():
+      _target,
+    # DatetimeRole(seasonality=["wd", "hour"], country="Nigeria"): "Timestamp",
+    CategoryRole(str):
+      "TransactionType",
+    CategoryRole(bool, label_encoded=True): ["WithdrawAll", "OneMillion"]
   }
   return df, roles
 
 
 @task
-def init(config: dict[str, Any]) -> None:
-  logger = prefect.context.get("logger")
-
-  np.random.seed(config["random_state"])
-  tf.random.set_seed(config["random_state"])
-  torch.manual_seed(config["random_state"])
-  random.seed(config["random_state"])
-  torch.set_num_threads(config["n_threads"])
-
-  logger.info("Initialized RNG seeds")
-
-
-@task
-def load_config() -> dict[str, Any]:
+def load_config(*, logger=None) -> dict[str, Any]:
   logger = prefect.context.get("logger")
 
   config = {
-    "target": "isFraud",
-    "n_threads": psutil.cpu_count(logical=False),
-    "test_size": 0.2,
+    "target": _target,
+    "n_threads": psutil.cpu_count(logical=True),
+    "test_size": FLAGS.test_size,
     "random_state": 42,
     "cv_folds": FLAGS.cv_folds,
-    "timeout": 3600,
+    "timeout": FLAGS.timeout,
   }
-  logger.info(f"Setting target column to {config['target']}")
-  logger.info(f"Setting number of threads to {config['n_threads']}")
-  logger.info(f"Setting number of CV folds to {config['cv_folds']}")
+  if logger:
+    logger.info(f"Setting target column to {config['target']}")
+    logger.info(f"Setting number of threads to {config['n_threads']}")
+    logger.info(f"Setting number of CV folds to {config['cv_folds']}")
   return config
 
 
 @task
-def split_data(df: pd.DataFrame,
-               config: dict[str, Any]) -> tuple[pd.DataFrame, pd.DataFrame]:
-  logger = prefect.context.get("logger")
-  train_df, test_df = train_test_split(
-    df,
-    test_size=config["test_size"],
-    random_state=config["random_state"],
-    stratify=df[config["target"]]
-  )
-  logger.info(
-    f"Splitted data train/test: {1-config['test_size']}/{config['test_size']}"
-  )
-  return train_df, test_df
+def init(config: dict[str, Any], *, logger=None) -> None:
+  np.random.seed(config["random_state"])
+  torch.manual_seed(config["random_state"])
+  random.seed(config["random_state"])
+  torch.set_num_threads(config["n_threads"])
+  if logger:
+    logger.info("Initialized RNG seeds")
 
 
 @task
 def first_level_pipeline(config) -> list[MLPipeline]:
-  lgbm_model0 = BoostLGBM(
+  gbt_model = BoostLGBM(
+    name="LGBM_GBDT",
     default_params={
       "boosting": "gbdt",
-      "early_stopping_rounds": 25,
-      "learning_rate": 0.025,
-      "num_leaves": 64,
+      "early_stopping_rounds": FLAGS.patience,
+      "learning_rate": FLAGS.lr,
+      "num_leaves": FLAGS.num_leaves,
       "num_threads": config["n_threads"],
       "num_trees": FLAGS.max_trees,
       "random_state": 0,
     }
   )
 
-  lgbm_model1 = BoostLGBM(
+  dart_model = BoostLGBM(
+    name="LGBM_DART",
     default_params={
       "boosting": "dart",
-      # "early_stopping_rounds": 25, Not supported in DART
-      "learning_rate": 0.025,
-      "num_leaves": 64,
+      # "early_stopping_rounds": FLAGS.patience, Not supported in DART
+      "learning_rate": FLAGS.lr,
+      "num_leaves": FLAGS.num_leaves,
       "num_threads": config["n_threads"],
       "num_trees": FLAGS.max_trees,
       "random_state": 1,
     }
   )
 
-  lgbm_model2 = BoostLGBM(
+  rf_model = BoostLGBM(
+    name="LGBM_RF",
     default_params={
       "boosting": "rf",
-      "early_stopping_rounds": 25,
-      "learning_rate": 0.025,
-      "num_leaves": 64,
+      "early_stopping_rounds": FLAGS.patience,
+      "learning_rate": FLAGS.lr,
+      "num_leaves": FLAGS.num_leaves,
       "num_threads": config["n_threads"],
       "num_trees": FLAGS.max_trees,
       "random_state": 2,
     }
   )
 
-  lgbm_model3 = BoostLGBM(
+  goss_model = BoostLGBM(
+    name="LGBM_GOSS",
     default_params={
       "boosting": "goss",
       "bagging_freq": 0,  # GOSS does not support bagging.
-      "early_stopping_rounds": 25,
-      "learning_rate": 0.025,
-      "num_leaves": 64,
+      "early_stopping_rounds": FLAGS.patience,
+      "learning_rate": FLAGS.lr,
+      "num_leaves": FLAGS.num_leaves,
       "num_threads": config["n_threads"],
       "num_trees": FLAGS.max_trees,
       "random_state": 3,
     }
   )
 
-  cb_model0 = BoostCB(
+  cb_model = BoostCB(
+    name="CB",
     default_params={
-      "learning_rate": 0.025,
+      "learning_rate": FLAGS.lr,
       "num_trees": FLAGS.max_trees,
       "random_state": 4,
       "thread_count": config["n_threads"],
@@ -171,7 +185,7 @@ def first_level_pipeline(config) -> list[MLPipeline]:
   gbt_pipeline = MLPipeline(
     [
       (
-        cb_model0,
+        gbt_model,
         OptunaTuner(
           n_trials=FLAGS.trials,
           timeout=config["timeout"],
@@ -179,7 +193,23 @@ def first_level_pipeline(config) -> list[MLPipeline]:
         ),
       ),
       (
-        lgbm_model0,
+        cb_model,
+        OptunaTuner(
+          n_trials=FLAGS.trials,
+          timeout=config["timeout"],
+          fit_on_holdout=False
+        ),
+      ),
+      # (
+      #   dart_model  # Too slow to run tuning
+      #   # OptunaTuner(
+      #   #   n_trials=FLAGS.trials,
+      #   #   timeout=config["timeout"],
+      #   #   fit_on_holdout=False
+      #   # ),
+      # ),
+      (
+        rf_model,
         OptunaTuner(
           n_trials=FLAGS.trials,
           timeout=config["timeout"],
@@ -187,23 +217,7 @@ def first_level_pipeline(config) -> list[MLPipeline]:
         ),
       ),
       (
-        lgbm_model1,
-        OptunaTuner(
-          n_trials=FLAGS.trials,
-          timeout=config["timeout"],
-          fit_on_holdout=False
-        ),
-      ),
-      (
-        lgbm_model2,
-        OptunaTuner(
-          n_trials=FLAGS.trials,
-          timeout=config["timeout"],
-          fit_on_holdout=False
-        ),
-      ),
-      (
-        lgbm_model3,
+        goss_model,
         OptunaTuner(
           n_trials=FLAGS.trials,
           timeout=config["timeout"],
@@ -211,7 +225,7 @@ def first_level_pipeline(config) -> list[MLPipeline]:
         ),
       ),
     ],
-    features_pipeline=LGBSimpleFeatures(),
+    features_pipeline=LGBAdvancedPipeline(),
   )
 
   linear_pipeline = MLPipeline(
@@ -221,48 +235,19 @@ def first_level_pipeline(config) -> list[MLPipeline]:
   return [gbt_pipeline, linear_pipeline]
 
 
-@task
-def second_level_pipeline(config) -> list[MLPipeline]:
-  del config  # not used
-  return [MLPipeline([LinearLBFGS()])]
+def main(argv):
+  del argv  # unused
+  with Flow("AutoML") as flow:
+    config = load_config()
+    init(config)
+    df = load_data()
+    df, roles = feature_engineering(df)
+    train_df, test_df = split_data(df, config)
+    level1 = first_level_pipeline(config)
+    automl = get_automl([level1], config)
+    train(automl, train_df, test_df, roles, config, logger=_logger)
+  flow.run()
 
 
-@task
-def get_model(pipelines, config):
-  task = lightautoml.tasks.Task("binary")
-  reader = PandasToPandasReader(
-    task, cv=config["cv_folds"], random_state=config["random_state"]
-  )
-
-  return AutoML(reader, pipelines)
-
-
-@task
-def train(model, train_df, test_df, roles, config):
-  logger = prefect.context.get("logger")
-
-  oof_pred = model.fit_predict(train_df, roles=roles, verbose=3)
-  test_pred = model.predict(test_df)
-
-  logger.info(
-    f"OOF score: "
-    f"{roc_auc_score(train_df[config['target']].values, oof_pred.data[:, 0])}"
-  )
-
-  logger.info(
-    f"TEST score: "
-    f"{roc_auc_score(test_df[config['target']].values, test_pred.data[:, 0])}"
-  )
-
-
-result = LocalResult(dir="/tmp/flow-results")
-with Flow("AutoML", result=result) as flow_automl:
-  config = load_config()
-  init(config)
-  df, roles = load_data()
-  df, roles = feature_engineering(df)
-  train_df, test_df = split_data(df, config)
-  level1 = first_level_pipeline(config)
-  level2 = second_level_pipeline(config)
-  model = get_model([level1, level2], config)
-  train(model, train_df, test_df, roles, config)
+if __name__ == "__main__":
+  app.run(main)
